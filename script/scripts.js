441582c124333bc8f86be70a0fc0a7b38cb4233f
newaz2580 <newaz2796@gmail.com>
2025-03-01 11:44:31 +0600

    added html tailwind css and js file

---
+++ script/scripts.js
@@ -0,0 +1,77 @@
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+document.getElementById("theme-btn").addEventListener("click", function () {
+  document.getElementById("body").style.backgroundColor = getRandomColor();
+});
+
+document.getElementById("discover").addEventListener("click", function () {
+  window.location.href = "./question.html";
+});
+
+const date = new Date();
+
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const currentDayOfWeek = daysOfWeek[date.getDay()];
+
+const currentTime = date.toLocaleTimeString();
+const currentDate = date.getFullYear();
+
+document.getElementById("day").innerText = currentDayOfWeek;
+document.getElementById("date").innerText = currentDate;
+
+let decreaseCount = parseInt(
+  document.getElementById("decrease-count").innerText
+);
+let increaseCount = parseInt(document.getElementById("increase-btn").innerText);
+
+const allBtn = document.querySelectorAll(".complete-btn");
+for (let i = 0; i < allBtn.length; i++) {
+  const btn = allBtn[i];
+  btn.addEventListener("click", function () {
+    if (decreaseCount > 0) {
+      alert("Board updated Sucessfully");
+      decreaseCount--;
+      document.getElementById("decrease-count").innerText = decreaseCount;
+      increaseCount++;
+      document.getElementById("increase-btn").innerText = increaseCount;
+      btn.disabled = true;
+      btn.style.backgroundColor = "gray";
+    }
+
+    let allDisableBtn = [...allBtn].every((btn) => btn.disabled);
+    if (allDisableBtn) {
+      alert("Congrates!!! You have completed all the current Tasks");
+    }
+
+    const id = btn.getAttribute("data-id");
+    const heading = document.getElementById("heading" + id);
+    const headingText = heading.innerText;
+    const list = document.getElementById("list");
+    const li = document.createElement("li");
+    li.classList.add("bg-[#f4f7ff]");
+    li.classList.add("p-4");
+    li.classList.add("mt-2");
+    li.innerText = `You have complete the task add ${headingText} at ${currentTime}`;
+    list.appendChild(li);
+  });
+}
+
+document.getElementById("clear-btn").addEventListener("click", function () {
+  const list = document.getElementById("list");
+  list.remove();
+});
